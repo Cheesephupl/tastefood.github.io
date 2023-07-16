4cd2c5e2d958541d206e120c74cd0af721499203
Cheesephu <73680470+Cheesephupl@users.noreply.github.com>
2023-07-17 00:06:23 +0700

    ss

--- js/js.js
+++ js/js.js
@@ -1,5 +1,27 @@
-const slides = document.querySelectorAll('.slide');
+window.addEventListener("scroll", function () {
+  var scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+  // Hiển thị hoặc ẩn nút bấm quay về top dựa trên vị trí cuộn của trang
+  if (window.pageYOffset > 100) {
+    scrollToTopBtn.style.display = "block";
+  } else {
+    scrollToTopBtn.style.display = "none";
+  }
+});
+
+// Xử lý sự kiện khi người dùng nhấp vào nút quay về top
+document.getElementById("scrollToTopBtn").addEventListener("click", function () {
+  // Cuộn lên đầu trang một cách mượt mà trong khoảng thời gian 500ms
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
+
+
+const slides = document.querySelectorAll('.slideshow-container img');
 let currentSlide = 0;
+let slideInterval;
 
 function showSlide(index) {
   slides.forEach((slide, i) => {
@@ -11,11 +33,15 @@ function showSlide(index) {
   });
 }
 
-function nextSlide() {
-  currentSlide = (currentSlide + 1) % slides.length;
+function changeSlide(n) {
+  clearInterval(slideInterval);
+  currentSlide = (currentSlide + n + slides.length) % slides.length;
   showSlide(currentSlide);
+  slideInterval = setInterval(() => {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+  }, 3000); // Change slide every 3 seconds
 }
 
 // Start the slideshow
-showSlide(currentSlide);
-setInterval(nextSlide, 3000); // Change slide every 3 seconds
+changeSlide(0);
