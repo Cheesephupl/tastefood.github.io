e70a901b145cc5721ec1466c4515a865bbd7a408
Cheesephu <73680470+Cheesephupl@users.noreply.github.com>
2023-07-16 17:23:00 +0700

    ipdate1

---
+++ js/js.js
@@ -0,0 +1,21 @@
+const slides = document.querySelectorAll('.slide');
+let currentSlide = 0;
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    if (i === index) {
+      slide.classList.add('active');
+    } else {
+      slide.classList.remove('active');
+    }
+  });
+}
+
+function nextSlide() {
+  currentSlide = (currentSlide + 1) % slides.length;
+  showSlide(currentSlide);
+}
+
+// Start the slideshow
+showSlide(currentSlide);
+setInterval(nextSlide, 3000); // Change slide every 3 seconds
